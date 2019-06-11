67a2364b6140c07b341b11018e5188c383ede1a6
Golubev Maxim <m2a2x2000@gmail.com>
2019-06-11 11:10:14 +0300

    favicon + plot + delete

--- prolog.py
+++ prolog.py
@@ -9,6 +9,23 @@ app = Sanic(log_config=None)
 
 import matplotlib.pyplot as plt
 
+def plot_data(data):
+    data = ([*zip(*data)])
+    x_plot = data[0]
+    a_plot = data[1]
+    b_plot = data[2]
+    e_plot = data[3]
+    e0_plot = data[4]
+    plt.subplot(221)
+    plt.plot(x_plot, a_plot)
+    plt.subplot(222)
+    plt.plot(x_plot, b_plot)
+    plt.subplot(223)
+    plt.plot(x_plot, e_plot)
+    plt.subplot(224)
+    plt.plot(x_plot, e0_plot)
+    plt.savefig('/app/static/prolog.png')
+
 def calc_prolog(x):
     a = np.float64(0.0)
     b = np.float64(0.0)
@@ -27,18 +44,21 @@ def calc_prolog(x):
         b = np.sqrt(a * b)
         e0 = e
         e = np.abs(a - b)
-    logger.info(data)
-    logger.info([*zip(*data)])
-    plt.plot([*zip(*data)])
-    plt.savefig('/app/static/prolog.png')
+    plot_data(data)
     return (x - 1.0) / a
-
 
 @app.route('/prolog')
 async def prolog(request):
     x = float(request.args["x"][0])
     y = calc_prolog(x)
     return json({"args": request.args, "answer": np.float64(y)})
+
+@app.route('/delete')
+async def prolog(request):
+    plt.subplot(111)
+    plt.plot([1],[1])
+    plt.savefig('/app/static/prolog.png')
+    return json({"success": "True"})
 
 # Serves files from the static folder to the URL /static
 app.static('/static', './static')
